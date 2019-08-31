9ef33a0817c018f22f17555c5586d6732285cdd3
kai <b10223052@yuntech.org.tw>
2019-08-31 18:58:07 +0800

    complete input-nickname page

--- src/pages/input-nickname/index.js
+++ src/pages/input-nickname/index.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, } from 'react-router-dom';
+import Container from '../../components/container';
 import './style.scss';
 
 const propTypes = {
 
 };
 
-class InputNicknamePage extends Component {
-	render() {
-		return (
-			<div>
-				Hello world
-			</div>
-		);
-	}
+function InputNicknamePage() {
+	return (
+		<Container className="input-nickname">
+			<p>暱稱聊天</p>
+			<input placeholder="輸入暱稱"></input>
+			<NavLink to="/select-mode"> [確定] </NavLink>
+		</Container>
+	);
 }
 
 InputNicknamePage.propTypes = propTypes;
