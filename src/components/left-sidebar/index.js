14fd31b97543ec3943a8dc33c654294c369a227f
kai <b10223052@yuntech.org.tw>
2019-09-01 16:42:52 +0800

    let left-side-bar be close

--- src/components/left-sidebar/index.js
+++ src/components/left-sidebar/index.js
@@ -6,67 +6,85 @@ import NewRoomModal from '../new-room-modal';
 import './style.scss';
 
 const propTypes = {
+	onClick: PropTypes.func,
+	isClose: PropTypes.bool,
+};
 
+const defaultProps = {
+	onClick: () => {},
 };
 
 class LeftSidebar extends Component {
 	
 	render() {
+		const { onClick, isClose } = this.props;
+
 		return (
 			<div className="left-sidebar">
-				<div className="left-sidebar__title">
+				<div 
+					style={ isClose ? { display: 'block' } : { display: 'none' } } 
+					onClick={onClick}
+					className="left-sidebar__title--close"
+				>
 					聊天大廳
 				</div>
-				<div className="left-sidebar__button-group">
-					<Button>隨機1對1配對</Button>
-					<Button>隨機進入群組</Button>
-				</div>
-				<input 
-					className="left-sidebar__input"
-					placeholder="搜尋聊天室名稱或編號"
-				></input>
-				<ChatRoom
-					title={"公開聊天室（11）"}
-					chatInfo={[
-						{
-							serialNumber: 'C001',
-							chatName: '聊天室名稱',
-							people: 3,
-							peopleOfMax: 15,
-						},
-						{
-							serialNumber: 'C001',
-							chatName: '聊天室名稱',
-							people: 3,
-							peopleOfMax: 15,
-						},
-						{
-							serialNumber: 'C001',
-							chatName: '聊天室名稱',
-							people: 3,
-							peopleOfMax: 15,
-						},
-					]}
-				/>
-				<ChatRoom
-					title={"公開聊天室（11）"}
-					chatInfo={[
-						{
-							serialNumber: 'C001',
-							chatName: '聊天室名稱',
-							people: 3,
-							peopleOfMax: 15,
-						},
-					]}
-				/>
-				<Button className="left-sidebar__button">
+				<div style={ isClose ? { display: 'none' } : { display: 'block' } }>
+					<div className="left-sidebar__title" onClick={onClick}>
+					聊天大廳
+					</div>
+					<div className="left-sidebar__button-group">
+						<Button>隨機1對1配對</Button>
+						<Button>隨機進入群組</Button>
+					</div>
+					<input 
+						className="left-sidebar__input"
+						placeholder="搜尋聊天室名稱或編號"
+					></input>
+					<ChatRoom
+						title={"公開聊天室（11）"}
+						chatInfo={[
+							{
+								serialNumber: 'C001',
+								chatName: '聊天室名稱',
+								people: 3,
+								peopleOfMax: 15,
+							},
+							{
+								serialNumber: 'C001',
+								chatName: '聊天室名稱',
+								people: 3,
+								peopleOfMax: 15,
+							},
+							{
+								serialNumber: 'C001',
+								chatName: '聊天室名稱',
+								people: 3,
+								peopleOfMax: 15,
+							},
+						]}
+					/>
+					<ChatRoom
+						title={"公開聊天室（11）"}
+						chatInfo={[
+							{
+								serialNumber: 'C001',
+								chatName: '聊天室名稱',
+								people: 3,
+								peopleOfMax: 15,
+							},
+						]}
+					/>
+					<Button className="left-sidebar__button">
 					新增聊天室
-				</Button>
+					</Button>
+				</div>
+
 			</div>
 		);
 	}
 }
 
 LeftSidebar.propTypes = propTypes;
+LeftSidebar.defaultProps = defaultProps;
 
 export default LeftSidebar;
--- src/pages/mode/index.js
+++ src/pages/mode/index.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Component, } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import loadComponent from '../../layout/lib/loadable';
 import LeftSidebar from '../../components/left-sidebar';
+import cx from 'classnames';
 import './style.scss';
 
 const Lobby = loadComponent({ loader: () => import('./lobby') });
 const GroupChart = loadComponent({ loader: () => import('./group-chat') });
 const OneToOneChat = loadComponent({ loader: () => import('./one-to-one-chat') });
 
-function ModePage() {
-	return (
-		<div className="mode-page">
-			<div className="mode-page__sidebar">
-				<LeftSidebar/>
+class ModePage extends Component {
+	constructor() {
+		super();
+		this.state = {
+			isClose: false,
+		};
+		this._handleToggleLeftBar = this._handleToggleLeftBar.bind(this);
+	}
+	_handleToggleLeftBar() {
+		this.setState({
+			isClose: !this.state.isClose,
+		});
+	}
+	render() {
+		const { _handleToggleLeftBar } = this;
+		const { isClose } = this.state;
+
+		return (
+			<div className={cx({ 'mode-page--close-side-bar': isClose }, 'mode-page')}>
+				<div className="mode-page__sidebar">
+					<LeftSidebar onClick={_handleToggleLeftBar} isClose={isClose}/>
+				</div>
+				<div className="mode-page__content">
+					<Switch>
+						<Route exact path="/mode/lobby" component={Lobby}></Route>
+						<Route exact path="/mode/gorup-chat" component={GroupChart}></Route>
+						<Route exact path="/mode/one-to-one-chat" component={OneToOneChat}></Route>
+					</Switch>
+				</div>
 			</div>
-			<div className="mode-page__content">
-				<Switch>
-					<Route exact path="/mode/lobby" component={Lobby}></Route>
-					<Route exact path="/mode/gorup-chat" component={GroupChart}></Route>
-					<Route exact path="/mode/one-to-one-chat" component={OneToOneChat}></Route>
-				</Switch>
-			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default ModePage;
