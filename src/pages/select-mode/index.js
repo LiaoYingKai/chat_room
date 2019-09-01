f74316d9eeac5e3a5f79ba7e561cd769b8bc8d74
kai <b10223052@yuntech.org.tw>
2019-09-02 00:17:28 +0800

    modify route link

--- src/pages/select-mode/index.js
+++ src/pages/select-mode/index.js
@@ -6,7 +6,7 @@ import './style.scss';
 function Home() {
 	return (
 		<Container className="select-mode">
-			<NavLink exact to="/mode/one-to-one-chat"> [隨機配對1對1聊天] </NavLink>
+			<NavLink exact to="/mode/chat-room"> [隨機配對1對1聊天] </NavLink>
 			<NavLink to="/mode/lobby"> [進入聊天大廳] </NavLink>
 		</Container>
 	);
