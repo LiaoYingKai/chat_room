ab69216183fed950dfe82333b34533b5100e7a5d
kai <b10223052@yuntech.org.tw>
2019-09-01 15:16:03 +0800

    add left-sidebar: title, button, input and css style

--- src/components/left-sidebar/index.js
+++ src/components/left-sidebar/index.js
@@ -13,7 +13,30 @@ class LeftSidebar extends Component {
 	render() {
 		return (
 			<div className="left-sidebar">
-				Hello world
+				<div className="left-sidebar__title">
+					聊天大廳
+				</div>
+				<div className="left-sidebar__button-group">
+					<Button>隨機1對1配對</Button>
+					<Button>隨機進入群組</Button>
+				</div>
+				<input 
+					className="left-sidebar__input"
+					placeholder="搜尋聊天室名稱或編號"
+				></input>
+				<div className="left-sidebar__chat-room">
+					<div className="left-sidebar__chat-room-title">
+
+					</div>
+				</div>
+				<div className="left-sidebar__chat-room">
+					<div className="left-sidebar__chat-room-title">
+
+					</div>
+				</div>
+				<Button className="left-sidebar__button">
+					新增聊天室
+				</Button>
 			</div>
 		);
 	}
