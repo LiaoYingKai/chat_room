f0e9ddc5c0b434eb78bf9175017c3618a3b32cfe
kai <b10223052@yuntech.org.tw>
2019-09-01 10:14:11 +0800

    add button component

---
+++ src/components/button/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+const propTypes = {
+	children: PropTypes.string,
+	onClick: PropTypes.func,
+	className: PropTypes.string,
+};
+
+const defaultProps = {
+	onClick: () => {},
+};
+
+
+function Button({ onClick, className, children }) {
+	return (
+		<button
+			onClick={onClick}
+			className={className}
+		>
+			{children}
+		</button>
+	);
+}
+
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
+
+export default Button;
